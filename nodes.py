0c3256f50f817d51afe5ee8a6291b6c7c463fdb0
Eric Beyer <eric@treat.xyz>
2024-04-15 15:08:34 -0400

    2

--- nodes.py
+++ nodes.py
@@ -1,12 +1,8 @@
 from copy import deepcopy
 import comfy.samplers
 import torch
-import math
 
 original_sampling_function = deepcopy(comfy.samplers.sampling_function)
-minimum_sigma_to_disable_uncond = 1
-no_uncond_at_all = False
-
 
 class GuidanceInterval:
     @classmethod
@@ -15,7 +11,7 @@ class GuidanceInterval:
                                 "model": ("MODEL",),
                                 "guidance_weight": ("FLOAT", {"default": 16.0, "min": 0.0, "max": 50.0, "step": 0.1}),
                                 "sigma_min": ("FLOAT", {"default": 0.28, "min": 0.0, "max": 1.0, "step": 0.01}),  
-                                "sigma_max": ("FLOAT", {"default": 5.42, "min": 0.0, "max": 10.0, "step": 0.01}),
+                                "sigma_max": ("FLOAT", {"default": 5.42, "min": 0.0, "max": 80.0, "step": 0.01}),
                               }}
     RETURN_TYPES = ("MODEL",)
     FUNCTION = "patch"
@@ -24,23 +20,21 @@ class GuidanceInterval:
 
     def patch(self, model, guidance_weight, sigma_min, sigma_max):
         def guidance_function(args):
-            sigma = args["sigma"][0]
+            sigma = args["sigma"]
             cond_denoised = args["cond_denoised"]
             uncond_denoised = args["uncond_denoised"]
-            x_in = args["input"]
 
             if sigma_min < sigma <= sigma_max:
                 # Apply guidance in interval
                 denoised = uncond_denoised + guidance_weight * (cond_denoised - uncond_denoised)
             else:
-                # Disable guidance outside interval
+                # Disable guidance outside interval  
                 denoised = uncond_denoised
 
-            # Clip the denoised image to a valid range
+            # Clamp the denoised output
             denoised = torch.clamp(denoised, -1.0, 1.0)
 
-            # Compute the final output
-            return x_in - denoised
+            return denoised
 
         m = model.clone()
         m.set_model_sampler_cfg_function(guidance_function)
